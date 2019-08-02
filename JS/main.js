6419366f4e69debf9d11747bce76867f07f3926b
skioe <52713980+skioe@users.noreply.github.com>
2019-08-02 19:19:11 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,15 @@
-$(".intro__content").hover(
-    function () {
-        $(".intro__title").fadeIn(1700)
-    }
-);
+$(document).ready(function () {
+    $(".intro__content").hover(
+        function () {
+            $(".intro__title").fadeIn(1700)
+        }
+    );
+});
 
 $(".intro__title").hover(
     function () {
         $(".intro__title").animate(
-            {"top":"-265px"},
+            {"top":"-205%"},
             2000,
             "easeOutQuad",
             function () {
@@ -20,7 +22,7 @@ $(".intro__title").hover(
     }
 );
 
-$( ".arrow__container")
+$( ".arrow")
     .hover(
     function () {
         $(".arrow__container").animate(
@@ -35,15 +37,109 @@ $( ".arrow__container")
 )   .click(
     function () {
         $(".intro").slideUp(
-            1350
-        )
+            1700
+        );
+        $(".main").fadeIn(
+            500,
+            function () {
+                $(".card_1_com").fadeIn(
+                    700,
+                    function () {
+                        $(".card_2_com").fadeIn(
+                            700,
+                            function () {
+                                $(".card_3_com").fadeIn(
+                                    700,
+                                    function () {
+                                        $(".card_4_com").fadeIn(
+                                            700
+                                        ).css("display", "flex")
+                                    }
+                                ).css("display", "flex")
+                            }
+                        ).css("display", "flex")
+                    }
+                ).css("display", "flex")
+            }
+
+        );
     }
 );
 
+$(".card_3_com")
+    .click(
+        function () {
+            /*$(".row__huge").css({
+                "height": "120%"
+            })*/
+            $(".row__huge").animate(
+                {"height": "40%"},
+                600,
+                "swing",
+                function () {
+                    $(".card_3__title__wrap").animate(
+                        {"top":"-33%"},
+                        1000,
+                        "easeOutSine",
+                        function () {
+                            $(".card_3_list_wrap").fadeIn(
+                                700
+                            )
+                        }
+                    )
+                }
 
+            )
+        }
+    );
 
+$(".card_4_com")
+    .click(
+        function () {
+            $(".row_2").animate(
+                {"height": "35%"},
+                600,
+                "swing",
+                function () {
+                    $(".card_4__title__wrap").animate(
+                        {"top":"-33%"},
+                        1000,
+                        "easeOutSine",
+                        function () {
+                            $(".card_4_list_wrap").fadeIn(
+                                700
+                            )
+                        }
+                    )
+                }
 
+            )
+        }
+    );
+/*$(".card_3__title__wrap").click(
+    function () {
+        $(".card_3_list_wrap").fadeOut(
+            300,
+            function () {
+                $(".card_3__title__wrap").animate(
+                    {"top":"none"},
+                    400,
+                    "easeOutSine",
+                    function () {
+                        $(".card_3_com").animate(
+                            {"height": "100%"},
+                            600
+                        )
+                    }
+                )
+            }
 
+        )
+    }
+);*/
 
-
-    
+$(".arrow_card").click(
+    function () {
+        $(".card_3").slideUp
+    }
+)
